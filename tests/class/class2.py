5206139a04b50f790376451b127f1abca6a7450d
Roman Zelenin <roman.zelenin88@gmail.com>
2025-05-11 20:50:43 +0300

    добавил 2

--- tests/class/class2.py
+++ tests/class/class2.py
@@ -1,4 +1,6 @@
 import json
+from tkinter.font import names
+
 from trio import open_file
 #пример чтения файла
 # file_data = open('data.txt','r')
@@ -23,3 +25,30 @@ class ReadAndPrintFile ():
 data1 = ReadAndPrintFile('data.txt').ReadAndPrint()
 print(data1['Contry'])
 print(type(data1))
+
+
+# user_location=input()
+# user_time=input()
+# print("Current location is "+ user_location + " and time is " + user_time)
+
+
+a = input()
+b = input()
+if str.isdigit(a) and str.isdigit(b) :
+    while  c := input():
+        v = True if int(c) in range(int(a), int(b)) else False
+        if v == False :
+          break
+    print(v)
+a = input()
+list_list = []
+max_list = []
+
+for i in range(int(a)):
+    lista = input().strip()
+    max_list.append(max(lista))
+
+max_list=sorted(max_list,reverse=True)
+print(';'.join( max_list))
+
+
